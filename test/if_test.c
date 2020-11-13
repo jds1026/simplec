e3c77b71bf5f9ddc721417e57240d89029995b24
jds1026 <josephdsullivan95@gmail.com>
2020-11-12 19:26:30 -0600

    change filename

---
+++ test/if_test.c
@@ -0,0 +1,20 @@
+int main(void)
+{
+	int more = 8;
+	int less = 8;
+
+	if (less == more) {
+		if (less > 10) {
+			int var = 34;
+		}
+		else {
+			int red = 'r';
+		}
+		less = 7;
+	}
+	else {
+		more = 7;
+	}
+
+	return 0;
+}
