92792941d34d71adc54ea958ebb6eca1e699942b
jds1026 <josephdsullivan95@gmail.com>
2020-11-30 17:20:41 -0600

    fix AST_If_Node next/else attribute

--- src/ast.c
+++ src/ast.c
@@ -1,26 +1,20 @@
 #include "simplec.h"
 #include <stdlib.h>
+#include <string.h>
 
-AST_Node* create_AST_node()
-{
-	AST_Node* new_node = (AST_Node*)malloc(sizeof(AST_Node));
-	new_node           = NULL;
-
-	return new_node;
-}
-
-void add_to_stmt_node_list(AST_Node** head, AST_Node* stmt)
+void add_to_stmt_node_list(AST_Node** head, AST_Node* stmt_node)
 {
 	if (*head == NULL) {
-		(*head) = stmt;
+		(*head) = stmt_node;
 		return;
 	}
 
-	AST_Node* temp = (*head);
-	while (temp->next != NULL) {
-		temp = temp->next;
+	AST_Node* current = (*head);
+	while (current->next != NULL) {
+		current = current->next;
 	}
-	temp->next = stmt;
+
+	current->next = stmt_node;
 }
 
 AST_Node* create_AST_ID_node(HT_item* item)
@@ -33,13 +27,25 @@ AST_Node* create_AST_ID_node(HT_item* item)
 	return (AST_Node*)new_node;
 }
 
-AST_Node* create_AST_const_node(Data_Type data_type, char* value)
+AST_Node* create_AST_const_node(Data_Type data_type, Literal_Type literal)
 {
 	AST_Const_Node* new_node = malloc(sizeof(AST_Const_Node));
 
 	new_node->type      = CONST_NODE;
 	new_node->data_type = data_type;
-	new_node->value     = value;
+	
+	if (literal.type == T_INT) {
+		new_node->value.i = literal.val.i;
+	}
+	else if (literal.type == T_FLOAT) {
+		new_node->value.f = literal.val.f;
+	}
+	else if (literal.type == T_EXP) {
+		new_node->value.f = literal.val.f;
+	}
+	else if (literal.type == T_CHAR) {
+		new_node->value.c = literal.val.c;
+	}
 
 	return (AST_Node*)new_node;
 }
@@ -90,7 +96,6 @@ AST_Node* create_AST_arithm_node(Arithm_Op operator, AST_Node* left, AST_Node* r
 	new_node->operator = operator;
 	new_node->left     = left;
 	new_node->right    = right;
-	new_node->next     = NULL;
 
 	return (AST_Node*)new_node;
 }
@@ -124,10 +129,12 @@ void print_AST(AST_Node* node, FILE* fp)
 		return;
 	}
 
-	// assign node
+	/* assign node */
+
 	if (node->type == ASSIGN_NODE) {
+		fprintf(fp, "%11sASSIGNMENT STATEMENT\n", " ");
 		AST_Assign_Node* assign_temp = (AST_Assign_Node*)node;
-		fprintf(fp, "%11s>>> ASSIGN NODE \'=\' HAS CHILDREN <<<\n", " ");
+		fprintf(fp, "%11s>>> ASSIGN OPERATOR \'=\' HAS CHILDREN <<<\n", " ");
 		print_AST(assign_temp->loperand, fp);
 		print_AST(assign_temp->value, fp);
 
@@ -137,7 +144,8 @@ void print_AST(AST_Node* node, FILE* fp)
 		}
 	}
 
-	// arithm node
+	/* arithm node */
+
 	if (node->type == ARITHM_NODE) {
 		AST_Arithm_Node* arithm_temp = (AST_Arithm_Node*)node;
 		char op;
@@ -156,14 +164,10 @@ void print_AST(AST_Node* node, FILE* fp)
 		fprintf(fp, "%11s>>> ARITHMIC OPERATOR \'%c\' HAS CHILDREN <<<\n", " ", op);
 		print_AST(arithm_temp->left, fp);
 		print_AST(arithm_temp->right, fp);
-
-		if (arithm_temp->next != NULL) {
-			fprintf(fp, "\n");
-			print_AST(arithm_temp->next, fp);
-		}
 	}
 
-	// relat node
+	/* relat node */
+
 	else if (node->type == RELAT_NODE) {
 		AST_Relat_Node* relat_temp = (AST_Relat_Node*)node;
 		char* op;
@@ -190,18 +194,24 @@ void print_AST(AST_Node* node, FILE* fp)
 		print_AST(relat_temp->right, fp);
 	}
 
-	// if node
+	/* if node */
+
 	else if (node->type == IF_NODE) {
+		fprintf(fp, "%11sIF STATEMENT", " ");
 		AST_If_Node* if_temp = (AST_If_Node*)node;
-		fprintf(fp,"\n%11s### IF NODE CONDITION ###\n", " ");
+		fprintf(fp,"\n%11s--- IF NODE CONDITION ---\n", " ");
 		print_AST(if_temp->condition, fp);
 
-		fprintf(fp, "\n%11s### IF NODE BRANCHES ###\n", " ");
+		fprintf(fp, "\n%11s=== IF NODE BRANCHES ===\n", " ");
 		print_AST(if_temp->if_branch, fp);
 
 		if (if_temp->else_branch != NULL) {
-			fprintf(fp, "\n%11s### ELSE NODE BRANCHES ###\n", " ");
+			fprintf(fp, "\n%11s=== ELSE NODE BRANCHES ===\n", " ");
 			print_AST(if_temp->else_branch, fp);
+			fprintf(fp, "%11sEND OF IF/ELSE\n", " ");
+		}
+		else {
+			fprintf(fp, "%11sEND OF IF\n", " ");
 		}
 
 		if (if_temp->next != NULL) {
@@ -210,14 +220,17 @@ void print_AST(AST_Node* node, FILE* fp)
 		}
 	}
 
-	// while node
+	/* while node */
+
 	else if (node->type == WHILE_NODE) {
+		fprintf(fp, "%11sWHILE STATEMENT", " ");
 		AST_While_Node* while_temp = (AST_While_Node*)node;
-		fprintf(fp, "\n%11s### WHILE NODE CONDITION ###\n", " ");
+		fprintf(fp, "\n%11s--- WHILE NODE CONDITION ---\n", " ");
 		print_AST(while_temp->condition, fp);
 
-		fprintf(fp, "\n%11s### WHILE NODE BRANCHES ###\n"," ");
+		fprintf(fp, "\n%11s=== WHILE NODE BRANCHES ===\n"," ");
 		print_AST(while_temp->while_branch, fp);
+		fprintf(fp, "%11sEND OF WHILE\n", " ");
 
 		if (while_temp->next != NULL) {
 			fprintf(fp, "\n");
@@ -225,26 +238,38 @@ void print_AST(AST_Node* node, FILE* fp)
 		}
 	}
 
-	// ID node
+	/* ID node */
+
 	else if (node->type == ID_NODE) {
 		AST_ID_Node* id_temp = (AST_ID_Node*)node;
 		fprintf(fp, "%15sID NODE %s\n", " ", id_temp->item->lexeme);
 	}
 
-	// const node
+	/* const node */
+
 	else if (node->type == CONST_NODE) {
 		AST_Const_Node* const_temp = (AST_Const_Node*)node;
 		char* type;
-		if (const_temp->data_type == T_NUM) {
-			type = "number";
+		if (const_temp->data_type == T_INT) {
+			type = "integer";
+			fprintf(fp, "%15sCONST NODE %s VALUE %d\n", " ", type, const_temp->value.i);
+		}
+		else if (const_temp->data_type == T_FLOAT) {
+			type = "float";
+			fprintf(fp, "%15sCONST NODE %s VALUE %f\n", " ", type, const_temp->value.f);
+		}
+		else if (const_temp->data_type == T_EXP) {
+			type = "float";
+			fprintf(fp, "%15sCONST NODE %s VALUE %e\n", " ", type, const_temp->value.f);
 		}
 		else if (const_temp->data_type == T_CHAR) {
 			type = "character";
+			fprintf(fp, "%15sCONST NODE %s VALUE %c\n", " ", type, const_temp->value.c);
 		}
-		fprintf(fp, "%15sCONST NODE %s VALUE %s\n", " ", type, const_temp->value);
 	}
 
-	// return node
+	/* return node */
+
 	else if (node->type == RETURN_NODE) {
 		AST_Return_Node* return_temp = (AST_Return_Node*)node;
 		fprintf(fp, "%11s>>> MAIN EXITED WITH RETURN VALUE %s <<<\n", " ", return_temp->ret_val);
@@ -276,6 +301,11 @@ void free_AST(AST_Node* head)
 		free_AST(if_temp->condition);
 		free_AST(if_temp->if_branch);
 		free_AST(if_temp->else_branch);
+
+		if (if_temp->next != NULL) {
+			free_AST(if_temp->next);
+		}
+
 		free(if_temp);
 	}
 	else if (node->type == ID_NODE) {
@@ -288,12 +318,22 @@ void free_AST(AST_Node* head)
 		AST_While_Node* while_temp = (AST_While_Node*)node;
 		free_AST(while_temp->condition);
 		free_AST(while_temp->while_branch);
+
+		if (while_temp->next != NULL) {
+			free_AST(while_temp->next);
+		}
+
 		free(while_temp);
 	}
 	else if (node->type == ASSIGN_NODE) {
 		AST_Assign_Node* assign_temp = (AST_Assign_Node*)node;
 		free_AST(assign_temp->loperand);
 		free_AST(assign_temp->value);
+
+		if (assign_temp->next != NULL) {
+			free_AST(assign_temp->next);
+		}
+
 		free(assign_temp);
 	}
 	else if (node->type == RETURN_NODE) {
